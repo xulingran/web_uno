1fb8dc03407d5470a03ec45676fa0cb50e0d151d
Zhong <lingchen2020@outlook.com>
2026-05-25 11:42:45 +0800

    feat(routing): add lobby route and mode-aware game initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import GamePage from "@/pages/GamePage";
 import SettingsPage from "@/components/SettingsPage";
+import LobbyPage from "@/pages/LobbyPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/lobby" element={<LobbyPage />} />
         <Route path="*" element={<GamePage />} />
       </Routes>
     </Router>
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,8 +1,15 @@
 import GameBoard from '@/components/GameBoard'
 import { useGameEngine } from '@/hooks/useGameEngine'
+import { useLobbyStore } from '@/store/lobbyStore'
 
 export default function GamePage() {
+  const networkMode = useLobbyStore((s) => s.networkMode)
+
   useGameEngine()
+
+  if (networkMode === 'client') {
+    return <GameBoard />
+  }
 
   return <GameBoard />
 }
